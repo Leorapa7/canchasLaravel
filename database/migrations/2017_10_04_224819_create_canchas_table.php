27e36b8d4d156fd5307ea0040ec25d8c1f749862
leorapa7 <leonel.rapagnani@davinci.edu.ar>
2017-10-04 20:23:08 -0300

    Commit Database Migrate

---
+++ database/migrations/2017_10_04_224819_create_canchas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCanchasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('canchas', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->string('nombre');
+            $table->float('latitud');
+            $table->float('longitud');
+            $table->integer('tamanio');
+            $table->float('precio_dia');
+            $table->float('precio_noche');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('canchas');
+    }
+}
---
+++ database/migrations/2017_10_04_230220_create_reservas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('fecha');
+            $table->string('horario');
+            $table->string('estado');
+            $table->integer('codReseva');
+            $table->integer('user_id')->unsigned();
+            $table->integer('cancha_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('cancha_id')->references('id')->on('canchas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservas');
+    }
+}
